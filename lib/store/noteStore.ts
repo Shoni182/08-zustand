298de364b86b34af3a37ea7c1be8f8d056e8e3cd
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-03-05 12:15:17 +0200

    add draft component

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { NewNote } from "@/types/note";
-import { NoteTag } from "@/types/note";
+import { persist } from "zustand/middleware";
 
 type NoteDraftStore = {
   draft: NewNote;
@@ -14,8 +14,18 @@ const initialDraft: NewNote = {
   tag: "Todo",
 };
 
-export const useNoteDraftStore = create<NoteDraftStore>()((set) => ({
-  draft: initialDraft,
-  setDraft: (note) => set(() => ({ draft: note })),
-  clearDraft: () => set(() => ({ draft: initialDraft })),
-}));
+export const useNoteDraftStore = create<NoteDraftStore>()(
+  persist(
+    (set) => ({
+      draft: initialDraft,
+      setDraft: (note) => set(() => ({ draft: note })),
+      clearDraft: () => set(() => ({ draft: initialDraft })),
+    }),
+    {
+      // Ключ у localStorage
+      name: "note-draft",
+      // Зберігаємо лише властивість draft
+      partialize: (state) => ({ draft: state.draft }),
+    },
+  ),
+);
